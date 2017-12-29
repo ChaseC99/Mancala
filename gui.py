69473175201663a39c214b211b9cbca7943c7266
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-12-29 12:30:07 -0800

    Finished update method to print board

--- gui.py
+++ gui.py
@@ -3,4 +3,24 @@
 #
 # Created by Chase Carnaroli
 
-class 
+class Console_UI():
+    def get_move(self) -> int:
+        try:
+            return int(input('Enter pit: '))
+        except:
+            return self.get_move()
+
+
+    def update(self, pits: [int]):
+        'Prints the board'
+        # Print first row
+        print('  ', end='')
+        print(pits[12], pits[11], pits[10], pits[9], pits[8], pits[7], sep=' ')
+
+        # Print the stores, with a gap inbetween
+        print(pits[13], ' ' * 11, pits[6])
+
+        # Print second row
+        print('  ', end='')
+        for pit in pits[:6]:
+            print(pit, end=' ')
