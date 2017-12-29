ecc4937467eae6e3fff4c1c3080d87a46de4d77e
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-12-29 12:06:29 -0800

    Finished game loop

--- mancala.py
+++ mancala.py
@@ -16,10 +16,39 @@ def start_game() -> None:
     # Create game
     game = model.Game()
 
-    play = True
+    game_over = False
 
-    while play:
-        # Do game
+    while not game_over:
+        # Switch turn
+        game.switch_turns() 
+        
+        # Preform Move
+        move_over = False
+        while not move_over:
+            # Get valid move
+            while True:
+                move = ui.get_move()
+                if game.check_valid_move(move):
+                    break
+
+            move_status = game.make_move(move)
+            move_over = move_status[0]
+            ending_pos = move_status[1]
+
+            ui.update(game.pits)
+
+            # See if player stole opponent pieces
+            steal_success = game.attempt_steal(ending_pos)
+
+            if steal_success:
+                ui.steal(ending_pos)
+        
+         # See if game is over
+         game_over = game.check_game_over()
+
+    print("Game Over")
+        
+        
 
 
 if __name__ == '__main__':
